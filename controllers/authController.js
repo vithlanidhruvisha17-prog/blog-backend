112d13af63b3a91d02d4e6feae37a7f6530b013e
vithlanidhruvisha17-prog <vithlanidhruvisha17@gmail.com>
2026-05-23 16:14:16 +0530

    changed nodemailer port to 587 for render

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,12 +71,17 @@ exports.forgotPassword = async (req, res) => {
         await user.save();
 
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
+    host: 'smtp.gmail.com',
+    port: 587,         
+    secure: false,     
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
+    },
+    tls: {
+        rejectUnauthorized: false 
+    }
+});
 
         const mailOptions = {
             from: `"BlogHub PRO" <${process.env.EMAIL_USER}>`,
