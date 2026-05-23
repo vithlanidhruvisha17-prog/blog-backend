96859827d1ed0d810ffc4c116bd1e08700f481e2
vithlanidhruvisha17-prog <vithlanidhruvisha17@gmail.com>
2026-05-23 18:27:11 +0530

    back to gmail config for final test

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,15 +70,13 @@ exports.forgotPassword = async (req, res) => {
         user.isOTPVerified = false; 
         await user.save();
 
-        const transporter = nodemailer.createTransport({
-        host: 'smtp-relay.brevo.com', 
-        port: 587,                    
-        secure: false,                 
-        auth: {
-            user: 'a68535001@smtp-brevo.com', 
-            pass: 'swjvzN85maQREyMV'        
-        }
-    });
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
+    }
+});
 
         const mailOptions = {
             from: `"BlogHub PRO" <${process.env.EMAIL_USER}>`,
